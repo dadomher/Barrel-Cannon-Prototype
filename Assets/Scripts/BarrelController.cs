9e8fc1e90a44ecace9712f736f24e9f451b685cf
Daniel Domínguez Hernández <dadomher@gmail.com>
2018-04-30 20:19:32 +0200

    Primer Commit
    Mecánica principal implementada.

---
+++ Assets/Scripts/BarrelController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelController : MonoBehaviour {
+    public Sprite barrel_off, barrel_on;
+    public GameObject barDirection, playerShell;
+
+    GameObject barDirection_clone, playerShell_clone;
+    float xBarrel, yBarrel, rotBarrel;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Detectar si aprieta la tecla "ESPACIO"
+        if (this.GetComponent<SpriteRenderer>().sprite == barrel_on && Input.GetKeyDown(KeyCode.Space)) {
+            FirePlayer();
+        }
+    }
+
+    //Funcion para activar las propiedades del barril cuando contiene el personaje (sprite, direccion, etc)
+    void BarrelContainPlayer() {
+            this.GetComponent<SpriteRenderer>().sprite = barrel_on;
+            barDirection_clone = Instantiate(barDirection, transform) as GameObject;
+            if (this.GetComponent<Animator>().enabled == false) this.GetComponent<Animator>().enabled = true;
+    }
+
+    //Funcion para activar las propiedades del barril cuando NO contiene el personaje (sprite, direccion, etc)
+    void BarrelNotContainPlayer() {
+        this.GetComponent<Animator>().enabled = false;
+        this.GetComponent<SpriteRenderer>().sprite = barrel_off;
+        Destroy(barDirection_clone);
+    }
+
+        //Funcion para disparar al jugador
+    void FirePlayer() {
+        //Almacenamos ciertas propiedades del barril
+        xBarrel = transform.position.x;
+        yBarrel = transform.position.y;
+        rotBarrel = transform.eulerAngles.z;
+
+        BarrelNotContainPlayer();
+
+        setBarrelOutput();
+
+        //Instanciamos al jugador para que salga disparado
+        playerShell_clone = Instantiate(playerShell, new Vector3(xBarrel, yBarrel, transform.position.z), transform.rotation) as GameObject;
+    }
+
+    //Calculamos donde estara posicionado el jugador cuando sea disparado segun la rotacion del objeto
+    void setBarrelOutput() {
+        if (rotBarrel <= 360 && rotBarrel >= 271) {
+            xBarrel += 1.2f;
+            yBarrel += 1.2f;
+        } else if (rotBarrel <= 270 && rotBarrel >= 181) {
+            xBarrel += 1.2f;
+            yBarrel -= 1.2f;
+        } else if (rotBarrel <= 180 && rotBarrel >= 91) {
+            xBarrel -= 1.2f;
+            yBarrel -= 1.2f;
+        } else  {
+            xBarrel -= 1.2f;
+            yBarrel += 1.2f;
+        }
+    }
+
+    //Funcion para detectar la collision con el jugador
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("player")) {
+            //print(collision.gameObject);
+            Destroy(collision.gameObject);
+            BarrelContainPlayer();
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        //aplicamos una fuerza de salida
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * 400);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
